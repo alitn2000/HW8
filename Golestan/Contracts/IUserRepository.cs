d2075eae5f0a062a100d4bc076832b9e5a882aca
alitn2000 <alitn2000@gmail.com>
2024-10-24 10:09:55 +0330

    first commit for hw8

---
+++ Golestan/Contracts/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Golestan.Entities;
+
+namespace Golestan.Contracts
+{
+    public interface IUserRepository
+    {
+        List<User> GetUsers();
+        void Login(User user);
+        void Register(User user);
+
+        User GetCurrentUser();
+
+        List<User> GetInActiveUsers();
+
+        Result ActivateUser(int Id);
+    }
+}
